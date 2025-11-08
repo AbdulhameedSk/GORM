f697060194a387b5e4a3d56320525b7d14dfecb9
abdulhameedskk <shaikabdulhameedd@gmail.com>
2025-11-08 11:13:35 +0530

    creating and getting books

--- storage/main.go
+++ storage/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	//For logging messages or errors to console
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
-	"golang.org/x/mod/sumdb/storage"
 	"gorm.io/gorm"
 )
 
-type Book struct{
-	Author string `json:"author"`
-	Title  string `json:"title"`
+type Book struct {
+	Author    string `json:"author"`
+	Title     string `json:"title"`
 	Publisher string `json:"publisher"`
 }
 
@@ -20,11 +20,37 @@ type Repository struct {
 	DB *gorm.DB
 }
 
+func (r *Repository) CreateBooks(c *fiber.Ctx) error {
+	// Implementation for creating a book
+	var book Book
+	if err := c.BodyParser(&book); err != nil {
+		c.Status(400).JSON(fiber.Map{"error": "Cannot parse JSON"})
+		return err
+	}
+	err := r.DB.Create(&book).Error
+	if err != nil {
+		c.Status(500).JSON(fiber.Map{"error": "Could not create book"})
+		return err
+	}
+	return c.Status(201).JSON(book)
+}
+
+func (r *Repository) GetBooks(c *fiber.Ctx) error{
+	bookModels : =&[]models.Book{}
+	err := r.DB.Find(bookModels).Error
+	if err != nil {
+		c.Status(500).JSON(fiber.Map{"error": "Could not retrieve books"})
+		return err
+	}
+	return c.Status(200).JSON(bookModels)
+}
+
+
 func (r *Repository) SetupRoutes(app *fiber.App) {
 	api := app.Group("/api")
 	api.POST("/Create_books", r.CreateBooks)
 	api.DELETE("/Delete_books/:id", r.DeleteBooks)
-	api.GET("/Get_books/:id", r.GetBooks)
+	api.GET("/Get_books/:id", r.GetBookByID)
 	api.GET("/books", r.GetBooks)
 	api.PUT("/Update_books/:id", r.UpdateBooks)
 	// Additional routes can be added here
@@ -35,10 +61,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	db,err:=storage.newConnection(config)
-	if err!=nil{
-		log.Fatal("Could not connect to the database",err)
+
+	db, err := storage.newConnection(config)
+	if err != nil {
+		log.Fatal("Could not connect to the database", err)
 	}
 
 	r := Repository{
